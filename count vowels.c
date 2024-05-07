150acb9953185524808259f567601f66d5e32fc0
Ramcharan Kundena <160163532+Ramcharan10122005@users.noreply.github.com>
2024-05-08 01:01:33 +0530

    Add files via upload

---
+++ count vowels.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+
+int countvowels(char str[]){
+   int count =0;
+   
+   for(int i=0;str[i]!='\0';i++){
+    if (str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'){
+        count++;
+    }
+    
+   }
+return count;
+} 
+int main(){
+    char str[]="ramcharan";
+printf("%d",countvowels(str));
+}
